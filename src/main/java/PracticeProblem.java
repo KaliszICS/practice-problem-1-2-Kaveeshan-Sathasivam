f3a58e48c4df60b9ebde651143a559752bb748f2
Kaveeshan-Sathasivam <343279329@tdsb.ca>
2026-02-11 16:25:13 +0000

    Variables+Integers+doublespractice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,22 +10,41 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		int num1=2;
+		int num2=5;
+		int num3=3;
+		System.out.println(num1+num2+num3);
+		System.out.println(num1*num2*num3);
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int int1=10;
+		int int2=3;
+		System.out.println(int1/int2);
+		System.out.println(int1%int2);
+
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		double fracnum1=5.3;
+		double fracnum2=5.2;
+		System.out.println(fracnum1/fracnum2);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		int number1=2;
+		int number2=3;
+		int number3=4;
+		System.out.println((number1+number2)*number3);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		double numb=5.0;
+		System.out.println(numb+3.5);
 	}
 
 }
